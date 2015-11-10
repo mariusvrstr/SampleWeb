8027e852b4f686ad7ab4dcff45250cee30d8db7f
Marius Vorster <marius.vrstr@gmail.com>
2015-11-10 21:56:34 +0200

    Added Scaffolding for Responsive Section
    This sets the foundation of the responsive section of the website

---
+++ Spike.Web/Controllers/BootstrapController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Spike.Web.Controllers
+{
+    public class BootstrapController : Controller
+    {
+        // GET: Bootstrap
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GridSystem()
+        {
+            return View();
+        }
+
+        public ActionResult ResponsiveForms()
+        {
+            return View();
+        }
+
+        public ActionResult ResponsiveGrid()
+        {
+            return View();
+        }
+
+    }
+}
